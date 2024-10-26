27f6f650b99e57eda7fe6ddc0528a361f4a735a4
simon@artify.email <11393585+SbiCA@users.noreply.github.com>
2024-10-26 14:25:16 +0200

    feat: add working in memory example

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
 using System.Text.Json.Serialization;
+using Rebus.Bus;
+using Rebus.Config;
+using Rebus.Handlers;
+using Rebus.Retry.Simple;
+using Rebus.Routing.TypeBased;
+using Rebus.Serialization.Json;
+using Rebus.Transport.InMem;
 
 var builder = WebApplication.CreateSlimBuilder(args);
+
+
+builder.Services.AddRebus(configure =>
+{
+    configure.Transport(t => t.UseInMemoryTransport(new InMemNetwork(),"my-queue"));
+    configure.Options(o => o.RetryStrategy());
+    configure.Serialization(c => c.UseSystemTextJson( AppJsonSerializerContext.Default.Options));
+    configure.Routing(r => r.TypeBased().Map<SimpleCommand>("my-queue"));
+    return configure;
+});
+
+builder.Services.AddRebusHandler<MyHandler>();
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
@@ -25,11 +44,49 @@ todosApi.MapGet("/{id}", (int id) =>
         ? Results.Ok(todo)
         : Results.NotFound());
 
+var cancellationToken = app.Services.GetRequiredService<IHostApplicationLifetime>().ApplicationStopping;
+var bus = app.Services.GetRequiredService<IBus>();
+_ = Sender(cancellationToken, bus, app.Services.GetRequiredService<ILogger<Program>>());
 app.Run();
+
+
+static async Task Sender(CancellationToken cancellationToken, IBus bus, ILogger<Program> logger)
+{
+    var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
+    while (await timer.WaitForNextTickAsync(cancellationToken))
+    {
+        try
+        {
+            await bus.SendLocal(new SimpleCommand(Guid.NewGuid().ToString(), DateTime.Now));
+        }
+        catch (Exception e)
+        {
+           logger.LogError(e, "Failed to send command");
+        }
+    }
+}
 
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
 [JsonSerializable(typeof(Todo[]))]
+[JsonSerializable(typeof(SimpleCommand))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
+}
+
+public record SimpleCommand(string Id, DateTime CreatedAt);
+public class MyHandler  : IHandleMessages<SimpleCommand>
+{
+    private readonly ILogger<MyHandler> _logger;
+
+    public MyHandler(ILogger<MyHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(SimpleCommand message)
+    {
+        _logger.LogInformation("Received command with ID {Id} created at {CreatedAt}", message.Id, message.CreatedAt);
+        return Task.CompletedTask;
+    }
 }
